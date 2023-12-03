5a1323be5c83426049b7bdd3ee5c22f6dbadfce2
rotlerxd <nunezsierraa@gmail.com>
2023-12-03 19:26:20 +0100

    xd

--- test.asm
+++ test.asm
@@ -5,62 +5,61 @@ b: .word 10, 20
 .text
     no_ext:
             # To implement with RISC-V instructions (without extension)
-            lw s3, 0(a0)            # load the first part of the complex number a
-            lw s4, 4(a0)            # load the second part
-            lw s5, 0(a1)            # load the first part of the complex number b
-            lw s6, 4(a1)            # load the second part
+            lw s3, 0(a0)                # load the first part of the complex number a
+            lw s4, 4(a0)                # load the second part
+            lw s5, 0(a1)                # load the first part of the complex number b
+            lw s6, 4(a1)                # load the second part
             
             # if (a == b):
             bne s3 s5 not_equal_no_ext 
             bne s4 s6 not_equal_no_ext 
             
             # Calculate a * b;
-            mul s11 s3 s5           # multiply real parts
-            mul s10 s4 s6           # multiply imaginary parts
-            sub s11 s11 s10         # substract these two parts
+            mul s11 s3 s5               # multiply real parts
+            mul s10 s4 s6               # multiply imaginary parts
+            sub s11 s11 s10             # substract these two parts
 
-            mul s10 s3 s6           # multiply real part of a with imaginary part of b
-            mul s9 s4 s5            # multiply imaginary part of a with real part of b
-            add s10 s10 s9          # add these two parts
+            mul s10 s3 s6               # multiply real part of a with imaginary part of b
+            mul s9 s4 s5                # multiply imaginary part of a with real part of b
+            add s10 s10 s9              # add these two parts
 
-            mv a0 s11               # move the result to a0 
-            mv a1 s10               # and a1 to return it
+            mv a0 s11                   # move the result to a0 
+            mv a1 s10                   # and a1 to return it
 
-            ret                     # return a * b;
+            ret                         # return a * b;
 
             
             # else
             not_equal_no_ext: 
-            add a0 s3 s5            # add real parts
-            add a1 s4 s6            # add imaginary parts
+            add a0 s3 s5                # add real parts
+            add a1 s4 s6                # add imaginary parts
 
-            addi sp sp -8           # move the stack pointer
-            sw a0 0(sp)             # store the result of a0 
-            sw a1 4(sp)             # and a1 on the stack
-
-            ret                     # return a + b;
+            ret                         # return a + b;
 
 
 
     with_ext:
             # To implement with RISC-V instructions (with extension)
-            lc s3, s4, (a0)        # load the complex number a and
-            lc s5, s6, (a1)        # b using the new instruction
+            lc s3, s4, (a0)             # load the complex number a and
+            lc s5, s6, (a1)             # b using the new instruction
             
             # if (a == b):
             beqc s3, s4, s5, s6, not_equal_ext 
 
             # Calculate a * b;
-            mulc s3, s4, s5, s6     # multiply the two complex numbers using the new instruction
+            mulc s3, s4, s5, s6         # multiply the two complex numbers using the new instruction
+            
+            add a0 s3 zero              # move the results to a0
+            add a1 s4 zero              # and a1 to return them (using add because we dont have mv)
 
             ret                     
 
             # else
             not_equal_ext:
-            addc s3, s4, s5, s6     # add the two complex numbers using the new instruction
+            addc s3, s4, s5, s6         # add the two complex numbers using the new instruction
 
-            addi sp sp -8           # move the stack pointer
-            sc s3, s4, (sp)         # store the result of the operation using the new instruction
+            add a0 s3 zero              # move the results to a0
+            add a1 s4 zero              # and a1 to return them (using add because we dont have mv)
 
             ret                     
 
